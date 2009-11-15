90df74494afe9d68250d08ae9d48a0d775df65ae
Elijah Newren <newren@gmail.com>
2009-11-14 20:17:41 -0700

    tests: Disable gitweb tests; there's no need for us to run them

--- t/gitweb-lib.sh
+++ t/gitweb-lib.sh
@@ -65,6 +65,11 @@ if ! test_have_prereq PERL; then
 	test_done
 fi
 
+if ! test_have_prereq gitweb; then
+	say 'skipping gitweb tests, gitweb not available'
+	test_done
+fi
+
 perl -MEncode -e 'decode_utf8("", Encode::FB_CROAK)' >/dev/null 2>&1 || {
     say 'skipping gitweb tests, perl version is too old'
     test_done
